37cdfcd02b308db83a924b167169df83af833df2
VictorGuui <victorguilhermebranco@outlook.com>
2023-04-19 12:06:14 -0300

    First step on Spring

---
+++ src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -0,0 +1,11 @@
+package med.voll.api.medico;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PutMapping;
+
+public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
+
+    public  DadosListagemMedico(Medico medico){
+         this(medico.getId(),medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
